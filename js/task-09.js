0b95f0d7bedbfefcf413e2664e6a5873fea38620
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-02-12 10:17:24 +0200

    + д.з. 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const changeColor = document.querySelector('.widget');
+const btnChangeColor = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', changeBackgroundColor);
+function changeBackgroundColor(event) {
+  
+  span.textContent = getRandomHexColor();
+  changeColor.style.backgroundColor = span.textContent = getRandomHexColor();
+};
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
