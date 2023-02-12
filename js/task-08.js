94c3dde6cd2ea1a8916a66db72b6414a4a7f109d
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-02-12 09:43:59 +0200

    + д.з. 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formSubmst = document.querySelector('.login-form');
+
+formSubmst.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const el = event.currentTarget;
+    if(el.email.value === '' || el.password.value === '') {
+        return alert('Заповни вільні поля!');
+    }
+    const email = el.email.value;
+    const password = el.password.value;
+
+    const data = {
+        email,
+        password,
+    }
+    console.log(data);
+    el.reset();
+}
 
